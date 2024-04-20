d70a1724e9f02b8ecad5eba49206d5202d5e8db8
BDN <nhavriyanto@gmail.com>
2024-04-21 00:42:39 +0700

    setup

---
+++ src/test/java/com/bdn/springboot2nd/SpringBoot2ndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdn.springboot2nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2ndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
